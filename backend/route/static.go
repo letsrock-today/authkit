c01425df8aeb85f5db5d08870b889e0be5eda860
Andrey Ustinov <ustinov.post@gmail.com>
2016-09-17 20:16:37 +0300

    Rename reset-password.html

--- backend/route/static.go
+++ backend/route/static.go
@@ -5,6 +5,6 @@ import "github.com/labstack/echo"
 func initStatic(e *echo.Echo) {
 	e.File("/", "../ui-web/html/index.html")
 	e.File("/login", "../ui-web/html/login.html")
-	e.File("/password-reset", "../ui-web/html/reset-password.html")
+	e.File("/password-reset", "../ui-web/html/password-reset.html")
 	e.Static("/dist", "../ui-web/dist")
 }
