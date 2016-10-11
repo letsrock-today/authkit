aada2b8cf21b238567141dd0ecf15be33e9cf810
Nikolay Turpitko <nikolay@turpitko.com>
2016-10-11 14:44:21 +0700

    Some doc

--- authkit/handler/handler.go
+++ authkit/handler/handler.go
@@ -6,16 +6,35 @@ import (
 	"github.com/letsrock-today/hydra-sample/authkit/config"
 )
 
+// NewHandler returns default Handler implemetation.
 func NewHandler(c config.Config) Handler {
 	return handler{c}
 }
 
+//TODO: describe API in swagger format.
+
+// Handler combines http-handlers useful to create login logic.
 type Handler interface {
+
+	// AuthCodeURLs responds with auth code URLs for OAuth2.
+	// Handler takes slice of oauth2.Config from configuration supplied
+	// to NewHandler() func and renders list of URLs to response body.
+	// Web UI could use this request to update its list of providers with fresh
+	// URLs (re-generate state query parameter in them).
+	// Response should not be cached.
 	AuthCodeURLs(echo.Context) error
+
+	// AuthProviders responds with list of OAuth2 providers, configured by the
+	// application. Response could be used by web UI to represent a list of
+	// providers with names and icons. Response could be cached.
 	AuthProviders(echo.Context) error
 }
 
-//TODO: comment where the methods are implemented...
+//TODO: currently handler marshals response as JSON; we may provide setting
+// (marshalling func in config) to change response type (for ex. c.XML()).
+
+// handler implements Handler interface.
+// Note: methods are implemented in separate files.
 type handler struct {
 	config config.Config
 }
